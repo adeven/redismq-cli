08013f2995c0463e39f5edeecda0ac307e3e3022
Paul H. Müller <paul@adeven.com>
2013-11-18 21:15:44 +0100

    add delete command

---
+++ delete.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"github.com/adeven/redismq"
+	"os"
+)
+
+var cmdDelete = &Command{
+	Run:   runDelete,
+	Usage: "delete [queue name]",
+	Short: "delete a queue",
+	Long:  `Delete completely removes a queue from the database incl. all consumers.`,
+}
+
+func runDelete(cmd *Command, args []string) {
+	if len(args) != 1 {
+		cmd.printUsage()
+		os.Exit(2)
+	}
+
+	name := args[0]
+	queue, err := redismq.SelectQueue(RedisURL, RedisPassword, RedisDBInt, name)
+	if err != nil {
+		fmt.Printf("queue with the name %s does not exists\n", name)
+		os.Exit(2)
+	}
+
+	err = queue.Delete()
+	if err != nil {
+		fmt.Printf("error deleting the queue %s: %s\n", name, err)
+		os.Exit(2)
+	}
+	fmt.Printf("deleted queue with the name %s\n", name)
+}
